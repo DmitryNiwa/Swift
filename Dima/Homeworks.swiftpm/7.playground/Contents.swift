24d54d4bf3264a8a72623b80335b9385678349f8
dkachalin <dmitry.niwa@gmail.com>
2023-11-03 13:15:47 +0700

    Homework 7 fixed

--- Dima/Homeworks.swiftpm/7.playground/Contents.swift
+++ Dima/Homeworks.swiftpm/7.playground/Contents.swift
@@ -1,3 +1,41 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let (a1, a2, a3, a4, a5) = ("1", "2", "3", "4", "5")
+//Ищем сумму
+var sum: Int = 0
+for i in [a1, a2, a3, a4, a5] {
+    if Int(i) != nil {
+        sum += Int(i)!
+    }
+    else {
+        print("Something wrong with " + i)
+    }
+}
+//5 констант nil
+let n1: Int? = nil
+let (n2, n3, n4, n5): (String?, Double?, Float?, Bool?) = (nil, nil, nil, nil)
+
+
+//5 констант с опциональными типами и присваиваем им значения
+let (opt1, opt2, opt3, opt4, opt5):(Int?, String?, Double?, Float?, Bool?)
+
+(opt1, opt2, opt3, opt4, opt5) = (32, "s", 2.0, 3.0, true)
+opt1
+opt2
+opt3
+opt4
+opt5
+
+//Создать переменную строчную, в которую записать свое имя, а затем распечатать ее по буквам
+let Name = "Дмитрий"
+for i in Name {
+    print(i)
+}
+
+//Создать массив с целочисленными цифрами в хаотичном порядке. Распечатать в упорядоченном порядке эти цифры
+let massive = [23, 24, 1, 100, 2, 5]
+
+for i in massive.sorted() {
+    print(i)
+}
+
