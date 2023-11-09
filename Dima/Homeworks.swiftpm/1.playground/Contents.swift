ebbf31aa3245b6a12f43fbbe9f72cd3161436dc4
dkachalin <dmitry.niwa@gmail.com>
2023-11-09 23:52:40 +0700

    Homeworks for a pull request

---
+++ Dima/Homeworks.swiftpm/1.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//Int
+var int = 24
+
+//String
+var str = "string haha"
+
+//UInt
+var uint: UInt
+uint = 18446744073709551615 //соответствует UInt64
+var uint32 = UInt32.max
+var uint64 = UInt64.max
+///UInt32 vs UInt64
+///UInt 32 соответствует переменной интеджер, которая занимает 32 бита памяти, соответственно закодировать можно числа от 0 до 2^32
+///UInt 64 соответсвенно то же самое, но занимает 64 бита пасяти, кодируются числа от 0 до 2^64
+
+
+//Bool
+var boolValue: Bool
+boolValue = true
+
+//Float
+var floatingNumber = Float.pi
+//6 цифр после запятой
+
+//Double
+var doublingNumber = Double.pi
+//15 цифр после запятой
